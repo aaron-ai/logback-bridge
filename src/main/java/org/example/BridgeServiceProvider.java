760328e82c2233b97f92e6a7e8cf399c75dd57b9
Aaron Ai <yangkun.ayk@gmail.com>
2022-11-03 16:46:07 +0800

    Format code

--- src/main/java/org/example/BridgeServiceProvider.java
+++ src/main/java/org/example/BridgeServiceProvider.java
@@ -8,42 +8,43 @@ import org.apache.rocketmq.shaded.org.slf4j.spi.SLF4JServiceProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class BridgeServiceProvider implements SLF4JServiceProvider {
+public final class BridgeServiceProvider implements SLF4JServiceProvider {
 
     private static final Logger logger = LoggerFactory.getLogger(BridgeServiceProvider.class);
 
-    private static org.slf4j.spi.SLF4JServiceProvider findServiceProvider() {
+    private final org.slf4j.spi.SLF4JServiceProvider delegate;
+
+    public BridgeServiceProvider() {
         logger.debug("Try to find unshaded SLF4j service provider.");
         try {
             final Method method = LoggerFactory.class.getDeclaredMethod("getProvider");
             method.setAccessible(true);
-            final org.slf4j.spi.SLF4JServiceProvider provider =
-                (org.slf4j.spi.SLF4JServiceProvider) method.invoke(null);
+            this.delegate = (org.slf4j.spi.SLF4JServiceProvider) method.invoke(null);
             logger.debug("Found unshaded SLF4j service provider.");
-            return provider;
         } catch (Throwable t) {
             // Should never reach here.
             throw new RuntimeException("Failed to find unshaded SLF4j service provider.", t);
         }
+
     }
 
     public ILoggerFactory getLoggerFactory() {
-        return new ILoggerFactoryAdapter(findServiceProvider().getLoggerFactory());
+        return new ILoggerFactoryAdapter(delegate.getLoggerFactory());
     }
 
     public IMarkerFactory getMarkerFactory() {
-        return new IMarkerFactoryAdapter(findServiceProvider().getMarkerFactory());
+        return new IMarkerFactoryAdapter(delegate.getMarkerFactory());
     }
 
     public MDCAdapter getMDCAdapter() {
-        return new MDCAdapterAdapter(findServiceProvider().getMDCAdapter());
+        return new MDCAdapterAdapter(delegate.getMDCAdapter());
     }
 
     public String getRequestedApiVersion() {
-        return findServiceProvider().getRequestedApiVersion();
+        return delegate.getRequestedApiVersion();
     }
 
     public void initialize() {
-        findServiceProvider().initialize();
+        delegate.initialize();
     }
 }
